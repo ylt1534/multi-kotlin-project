7612b7425099e3170c189b01625d2fe3230b8dc3
yin_l <yin_l@worksap.co.jp>
2019-04-19 12:15:38 +0800

    replaced implementation with testImplementation

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.springframework.boot:spring-boot-starter-web:2.1.3.RELEASE")
     implementation(project(":cli"))
-    implementation(project(":config"))
+    testImplementation(project(":config"))
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.3.RELEASE")
 }

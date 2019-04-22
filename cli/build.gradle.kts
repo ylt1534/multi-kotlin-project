a3402bbcc9bd5420cfcc1bd023bbc6ca9370f1e5
yin_l <yin_l@worksap.co.jp>
2019-04-22 13:36:00 +0800

    package war

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
     kotlin("jvm")
+    war
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.30"
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":log-config"))
+    testRuntimeOnly("org.springframework.boot:spring-boot-starter-tomcat:2.1.3.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-web:2.1.3.RELEASE")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.3.RELEASE")
+}
+
+tasks.getByName<War>("war") {
+    archiveName = "cli.war"
+//    mainClassName = "wap.hr.recruiting.biz.masterdata.MasterDataMaintainenceServiceKt"
 }
--- cli/src/main/kotlin/hello/service/CliApplication.kt
+++ cli/src/main/kotlin/hello/service/CliApplication.kt
@@ -1,11 +1,16 @@
 package hello.service
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication(scanBasePackages = ["hello"])
-open class CliApplication
-
-fun main(args: Array<String>) {
-    SpringApplication.run(CliApplication::class.java, *args)
+class CliApplication: SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(CliApplication::class.java)
+    }
 }
+
+//fun main(args: Array<String>) {
+//    SpringApplication.run(CliApplication::class.java, *args)
+//}

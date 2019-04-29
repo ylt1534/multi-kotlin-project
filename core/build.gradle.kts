113d4c9842e1b1c38ba7119a3e1e6bde5b9b012c
yin_l <yin_l@worksap.co.jp>
2019-04-29 11:02:05 +0800

    config log-back in common config

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.springframework.boot:spring-boot-starter-web:2.1.3.RELEASE")
+    implementation(project(":log-config"))
     testImplementation(project(":config"))
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.3.RELEASE")
 }
--- core/src/test/kotlin/hello/app/TestMain.kt
+++ core/src/test/kotlin/hello/app/TestMain.kt
@@ -2,10 +2,10 @@ package hello.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.PropertySource
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootApplication(scanBasePackages = ["hello"])
-@PropertySource("classpath:config-application.properties")
+@TestPropertySource("classpath:config-application.properties")
 open class TestMain
 
 fun main(args: Array<String>) {
